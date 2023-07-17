60d4190602ee40092f58a96724f3ca5122eeb2e9
RTGuinn <>
2023-07-17 09:29:31 +0000

    Add code to generate a random choice for computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,27 @@
+// DOM content loading listener
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "rock") {
+                alert("You chose Rock!");
+            } else {
+                let playerChoice = this.getAttribute("data-type");
+                alert(`You chose ${playerChoice}`);
+            }
+        })
+    }
+})
+
+/**
+ * Main game loop, called when script is loaded and after the user
+ * has made their choice.
+ */
 function runGame() {
 
+    // call random computer choice
+    let computerChoice = getRandomCard();
 }
 
 function playerChoice() {
@@ -8,7 +30,22 @@ function playerChoice() {
 
 function computerChoice() {
 
+        // generate random card choice for computer
+        let getRandomCard = Math.floor(Math.random() * 3);
+         
+        // map the random number to rock paper scissor selection
+            switch (randomCard) {
+                case 0: 
+                    return "rock";
+                case 1:
+                    return "paper";
+                case 2:
+                    return "scissor";    
+
+            }
 }
+
+
 
 function compareChoice() {
 
@@ -19,5 +56,5 @@ function incrementWin() {
 }
 
 function incrementLoss() {
-    
+
 }
